508b96f2407dce14ec8c14eb26457ff0cfc87386
Kacper <kacper.siembab@gmail.com>
2024-09-17 22:59:17 +0200

    change SpaceMono font for Outfit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,40 @@
+import {
+	DarkTheme,
+	DefaultTheme,
+	ThemeProvider
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+	const colorScheme = useColorScheme();
+	const [loaded] = useFonts({
+		Outfit: require("../assets/fonts/Outfit-VariableFont_wght.ttf")
+	});
+
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
+
+	if (!loaded) {
+		return null;
+	}
+
+	return (
+		<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+			<Stack>
+				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+				<Stack.Screen name="+not-found" />
+			</Stack>
+		</ThemeProvider>
+	);
 }
