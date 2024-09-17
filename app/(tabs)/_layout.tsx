945ab35e11b553afb5895582b7743b920209ca2a
Kacper <kacper.siembab@gmail.com>
2024-09-18 00:03:47 +0200

    add color scheme tint color and tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,22 +4,34 @@ import React from "react";
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { View, Text } from "react-native";
 
 export default function TabLayout() {
 	const colorScheme = useColorScheme();
 
 	return (
-		<Tabs screenOptions={{ headerShown: false }}>
+		<Tabs
+			screenOptions={{
+				headerShown: false,
+				tabBarActiveTintColor: Colors[colorScheme ?? "light"].tabIconSelected
+			}}
+		>
 			<Tabs.Screen
 				name="index"
 				options={{
-					title: "Home"
+					title: "Home",
+					tabBarIcon: ({ color, size }) => (
+						<TabBarIcon name={"home"} color={color} size={size} />
+					)
 				}}
 			/>
 			<Tabs.Screen
 				name="account"
 				options={{
-					title: "Account"
+					title: "Account",
+					tabBarIcon: ({ color, size }) => (
+						<TabBarIcon name={"person"} color={color} size={size} />
+					)
 				}}
 			/>
 		</Tabs>
