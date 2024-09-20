817bb1d263eff577dd88add13a50a0562fa458b4
Kacper <kacper.siembab@gmail.com>
2024-09-20 17:15:43 +0200

    move index to (explore)

--- app/(tabs)/(explore)/index.tsx
+++ app/(tabs)/(explore)/index.tsx
@@ -1,20 +1,17 @@
+import { ThemedSafeView } from "@/components/ThemedSafeView";
 import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import { Stack } from "expo-router";
-import { Text, View } from "react-native";
 
 export default function Index() {
 	return (
 		<>
-			<ThemedView
+			<ThemedSafeView
 				style={{
 					flex: 1,
-					justifyContent: "center",
 					alignItems: "center"
 				}}
 			>
 				<ThemedText>Edit app/index.tsx to edit this screen.</ThemedText>
-			</ThemedView>
+			</ThemedSafeView>
 		</>
 	);
 }
