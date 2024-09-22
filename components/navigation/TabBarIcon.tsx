a21d128ab9b67f003651788ab6240d6b3d38b06c
Kacper <kacper.siembab@gmail.com>
2024-09-22 15:52:16 +0200

    add an Icon element to handle all icons in the project

--- components/navigation/TabBarIcon.tsx
+++ components/navigation/TabBarIcon.tsx
@@ -1,14 +1,12 @@
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 
-import { MaterialIcons } from "@expo/vector-icons";
 import { type IconProps } from "@expo/vector-icons/build/createIconSet";
 import { type ComponentProps } from "react";
+import { Icon } from "../Icon";
 
 export function TabBarIcon({
 	style,
 	...rest
-}: IconProps<ComponentProps<typeof MaterialIcons>["name"]>) {
-	return (
-		<MaterialIcons size={28} style={[{ marginBottom: -3 }, style]} {...rest} />
-	);
+}: IconProps<ComponentProps<typeof Icon>["name"]>) {
+	return <Icon size={28} style={[{ marginBottom: -3 }, style]} {...rest} />;
 }
