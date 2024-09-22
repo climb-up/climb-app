9bbe8b35db070daf5f663092ae0304411897411a
Kacper <kacper.siembab@gmail.com>
2024-09-22 15:52:34 +0200

    create ExploreMountainCard

---
+++ components/ExploreMountainCard.tsx
@@ -0,0 +1,60 @@
+import { View, StyleSheet } from "react-native";
+import { ThemedText } from "./ThemedText";
+import { ImageBackgroundWithGradient } from "./ImageBackgroundWithGradient";
+import { Icon } from "./Icon";
+
+type ExploreMountainCardProps = {
+	name: string;
+	location: string;
+	pathCount: number;
+	backgroundImage: string;
+};
+
+export function ExploreMountainCard(props: ExploreMountainCardProps) {
+	return (
+		<View style={styles.view}>
+			<ImageBackgroundWithGradient
+				source={{ uri: props.backgroundImage }}
+				innerStyle={styles.innerView}
+			>
+				<View
+					style={{
+						display: "flex",
+						flexDirection: "row",
+						justifyContent: "space-between"
+					}}
+				>
+					<View>
+						<ThemedText isContrast type="title">
+							{props.name}
+						</ThemedText>
+						<ThemedText isContrast>
+							<Icon name="location-pin" size={13} />
+							{props.location}
+						</ThemedText>
+					</View>
+					<View style={{ display: "flex", alignItems: "center" }}>
+						<ThemedText isContrast type="title">
+							{props.pathCount}
+						</ThemedText>
+						<ThemedText isContrast>dróg</ThemedText>
+					</View>
+				</View>
+			</ImageBackgroundWithGradient>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	view: {
+		width: "100%",
+		height: 200,
+		borderRadius: 16,
+		overflow: "hidden"
+	},
+	innerView: {
+		display: "flex",
+		justifyContent: "flex-end",
+		padding: 16
+	}
+});
